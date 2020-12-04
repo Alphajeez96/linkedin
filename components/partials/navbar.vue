1efc469b897eb9e0ce2b8694ddd609f47c01f219
Prince <chimeremezechukwudire@gmail.com>
2020-12-04 10:24:47 +0100

    folder structure added

---
+++ components/partials/navbar.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <b-navbar class="nav_bar " toggleable="lg" variant="info">
+      <div class="d-flex">
+
+<!-- Logo here -->
+        <div class="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+              class="global-nav__logo"
+            >
+              <title>LinkedIn</title>
+              <g>
+                <path
+                  d="M34,2.5v29A2.5,2.5,0,0,1,31.5,34H2.5A2.5,2.5,0,0,1,0,31.5V2.5A2.5,2.5,0,0,1,2.5,0h29A2.5,2.5,0,0,1,34,2.5ZM10,13H5V29h5Zm.45-5.5A2.88,2.88,0,0,0,7.59,4.6H7.5a2.9,2.9,0,0,0,0,5.8h0a2.88,2.88,0,0,0,2.95-2.81ZM29,19.28c0-4.81-3.06-6.68-6.1-6.68a5.7,5.7,0,0,0-5.06,2.58H17.7V13H13V29h5V20.49a3.32,3.32,0,0,1,3-3.58h.19c1.59,0,2.77,1,2.77,3.52V29h5Z"
+                  
+                ></path>
+              </g>
+            </svg>
+        </div>
+
+<!-- Search bar here -->
+<div class="mx-3">
+<form action="" method="POST">
+<div class="form-group position-relative">
+<input type="search" placeholder="search">
+<!-- Search Icon here -->
+<div class="position-bsolute">
+</div>
+</div>
+</form>
+</div>
+        
+      </div>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <div>
+          <nuxt-link to v-b-modal.my-modal
+            ><span class="reg">Register</span></nuxt-link
+          >
+        </div>
+
+        <div class="mx-4">
+          <nuxt-link to="/sign-in"><span class="reg">Login</span></nuxt-link>
+        </div>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style  scoped>
+.global-nav__logo{
+    fill: #0a66c2
+}
+.top_logo {
+  height: 4vh;
+}
+
+.spiral_line {
+  height: 150px;
+}
+.nav_bar {
+  background-color: #fff !important;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+}
+.navbar-expand-lg .navbar-nav {
+  display: -webkit-inline-box;
+}
+.navbar {
+  padding: .5rem 0;
+}
+.reg {
+  color: #070707;
+  font-family: "Neue Helvetica";
+  font-weight: 400;
+}
+a {
+  text-decoration: none;
+}
+
+.bg-info {
+  letter-spacing: 0px;
+}
+.navbar-light .navbar-nav .nav-link {
+  color: red !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+   <linkNav/>
+  </div>
+</template>
+
+<script>
+import linkNav from '~/components/partials/navbar'
+export default {
+  components:{
+    linkNav
+  }
+}
+</script>
+
+<style>
+
+</style>
